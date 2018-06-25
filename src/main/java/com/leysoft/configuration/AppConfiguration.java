8f20ace9556532ed3ec053e7a34326eb2f29fb3e
will.leyton <will.leyton@valid.com>
2018-06-25 17:32:03 -0500

    Added files

---
+++ src/main/java/com/leysoft/configuration/AppConfiguration.java
@@ -0,0 +1,28 @@
+package com.leysoft.configuration;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.leysoft.beans.MicroOneFallbackProvider;
+import com.leysoft.filter.CustomPostZuulFilter;
+import com.leysoft.filter.CustomPreZuulFilter;
+
+@Configuration
+public class AppConfiguration {
+	
+	@Bean
+	public CustomPreZuulFilter preFilter() {
+		return new CustomPreZuulFilter();
+	}
+	
+	@Bean
+	public CustomPostZuulFilter postFilter() {
+		return new CustomPostZuulFilter();
+	}
+	
+	@Bean
+	public FallbackProvider fallbackProvider() {
+		return new MicroOneFallbackProvider();
+	}
+}
---
+++ src/main/java/com/leysoft/filter/CustomPreZuulFilter.java
@@ -0,0 +1,38 @@
+package com.leysoft.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class CustomPreZuulFilter extends ZuulFilter {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(CustomPreZuulFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		LOGGER.info("request -> {} request uri -> {}", request, request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
